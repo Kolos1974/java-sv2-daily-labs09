0e042c97eda3ab273f580a003cb9c65059090fb8
Kolos1974 <karlovitz.kolos@gmail.com>
2022-01-04 00:35:39 +0100

    abstractclass task copied

---
+++ src/main/java/day02/abstactclass/Main.java
@@ -0,0 +1,17 @@
+package day02.abstactclass;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Character archer = new Archer(new Point(0, 0), new Random());
+
+        Character enemy = new Archer(new Point(1, 0), new Random());
+
+        archer.secondaryAttack(enemy);
+
+        System.out.println(enemy.getHitPoint());
+
+    }
+}
